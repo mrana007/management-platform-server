ead7249bc5f4911cc4c40497a4f2be05766ea5a9
mrana007 <rana.es.bc@gmail.com>
2023-12-22 01:12:09 +0600

    implement delete method to delete task

--- index.js
+++ index.js
@@ -39,7 +39,12 @@ async function run() {
         res.send(result);
     });
 
-    
+    app.delete('/tasks/:id', async (req, res) => { 
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = todoCollection.deleteOne(query);
+        res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
